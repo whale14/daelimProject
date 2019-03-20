2e7a6282872e28956054e0526bd87e39ff4a2075
SangGyu LEE <zzazzu14@google.com>
2019-03-20 18:43:46 +0900

    Intro 껍데기만 생성

--- app/src/main/java/com/androidproject/MainActivity.java
+++ app/src/main/java/com/androidproject/MainActivity.java
@@ -24,36 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = FirebaseFirestore.getInstance();
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", 25);
-                user.put("last", 26);
-                user.put("born", 1815);
-
-// Add a new document with a generated ID
-                db.collection("student")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                            }
-                        });
-            }
-        });
-
 
     }
 }
