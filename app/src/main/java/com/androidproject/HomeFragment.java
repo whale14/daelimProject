1d2c972fa2ad2d3d7050ea23ad47be82a8a44008
SangGyu LEE <zzazzu14@gmail.com>
2019-03-27 12:54:29 +0900

    HomeFragment toolbar 적용중

--- app/src/main/java/com/androidproject/HomeFragment.java
+++ app/src/main/java/com/androidproject/HomeFragment.java
@@ -1,18 +1,24 @@
 package com.androidproject;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -28,9 +34,13 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        MainActivity activity = (MainActivity) getActivity();
+
+        setHasOptionsMenu(true);
 
         Toolbar mToolbar = rootview.findViewById(R.id.toolbar_home);
-        SearchView searchView = rootview.findViewById(R.id.search_view);
+        assert activity != null;
+        activity.setSupportActionBar(mToolbar);
 
         RecyclerView recyclerView = rootview.findViewById(R.id.recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -46,4 +56,21 @@ public class HomeFragment extends Fragment {
 //        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(getActivity()).invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_home, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
