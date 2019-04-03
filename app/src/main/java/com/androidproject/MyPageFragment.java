acd2d3663549f7572174a8ee03aa15943d084f1e
HyeYeon <gpdus335@naver.com>
2019-04-03 11:29:52 +0900

    캘린더 뷰 수정 및 업로드

--- app/src/main/java/com/androidproject/MyPageFragment.java
+++ app/src/main/java/com/androidproject/MyPageFragment.java
@@ -10,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -19,6 +22,8 @@ import java.util.Objects;
  */
 public class MyPageFragment extends Fragment {
 
+    TextView selectView;
+    CalendarView calendar;
 
     public MyPageFragment() {
         // Required empty public constructor
@@ -30,6 +35,39 @@ public class MyPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_my_page, container, false);
+
+        selectView = rootView.findViewById(R.id.selectView);
+
+        //CalendarView 인스턴스 만들기
+
+        CalendarView calendar = rootView.findViewById(R.id.calendar);
+
+        selectView.setText("");
+
+        //리스너 등록
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+
+
+            @Override
+
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+
+                                            int dayOfMonth) {
+
+                // TODO Auto-generated method stub
+
+                /*
+                Toast.makeText(MyPageFragment.this, ""+year+"/"+(month+1)+"/"
+
+                        +dayOfMonth, Toast.LENGTH_LONG).show(); */
+                selectView.setText(""+year+"/"+(month+1)+"/" +dayOfMonth);
+
+            }
+
+        });
+
 
         return rootView;
     }
